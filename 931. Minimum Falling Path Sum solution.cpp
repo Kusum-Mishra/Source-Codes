75719a238893baa533202aa564d2657784310a13
Kusum Mishra <kusummishra5678@gmail.com>
2025-09-05 21:27:19 +0530

    **Added function to calculate minimum falling path sum in a matrix**

---
+++ 931. Minimum Falling Path Sum solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    //bottom up approach
+    //TC - O(n*n)
+    //SC - O(n*n)
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<vector<int>> t(n, vector<int>(n));
+
+        for(int col = 0; col < n; col++)
+            t[0][col] = matrix[0][col];
+
+
+        for(int row = 1; row < n; row++)
+        {
+            for(int col = 0; col < n; col++)
+            {
+                int a = INT_MAX, b = INT_MAX;
+                if(col-1 >= 0)
+                    a = t[row-1][col-1];
+                if(col+1 < n)
+                    b = t[row-1][col+1];
+
+                t[row][col] = matrix[row][col] + min({t[row-1][col], a, b});
+            }
+        }
+
+
+        int result = INT_MAX;
+        int lastRow = n-1;
+        for(int col = 0; col < n; col++)
+            result = min(result, t[lastRow][col]);
+
+        return result;
+    }
+};

7cd63a30f699180fdb7e3e510dd7a39452607b96
Kusum Mishra <kusummishra5678@gmail.com>
2025-09-12 22:18:44 +0530

    **Added function to determine if Alice wins based on vowel count in input string**

---
+++ 3227. Vowels Game in a String solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        int vowels = 0;
+        for(char& ch : s)
+        {
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                vowels++;
+        }
+
+        if(vowels >= 1)
+            return true;
+        else 
+            return false;
+    }
+};

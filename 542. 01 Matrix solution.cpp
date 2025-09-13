60e20d196c0d0674e031f129ff40a9c025dc4ffc
Kusum Mishra <kusummishra5678@gmail.com>
2025-09-13 19:34:58 +0530

    Added solution for 542. 01 Matrix

---
+++ 542. 01 Matrix solution.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    //TC - O(m * n)
+    //SC - O(m * n)
+    vector<vector<int>> directions{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        queue<pair<int, int>> q;
+        vector<vector<int>> result(m, vector<int>(n, -1));
+
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++){
+                if(mat[i][j] == 0){
+                    result[i][j] = 0;
+                    q.push({i, j});
+                }
+            }
+        }
+
+        while(!q.empty())
+        {
+            pair<int, int> curr = q.front();
+            q.pop();
+            int i = curr.first;
+            int j = curr.second;
+
+            for(auto& dir : directions)
+            {
+                int new_i = i + dir[0];
+                int new_j = j + dir[1];
+                if(new_i >= 0 && new_i < m && new_j >= 0 && new_j < n && result[new_i][new_j] == -1){
+                    q.push({new_i, new_j});
+                    result[new_i][new_j] = 1 + result[i][j];
+                }
+            }
+        }
+
+        return result;
+    }
+};

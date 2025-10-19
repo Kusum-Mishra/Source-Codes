5c8f1c3d706fdc4145d4ec5c7c5c41a4e23c96a7
Kusum Mishra <kusummishra5678@gmail.com>
2025-10-19 13:14:07 +0530

    Added solution for 455. Assign Cookies

--- 455. Assign Cookies solution.cpp
+++ 455. Assign Cookies solution.cpp
@@ -1,50 +1,17 @@
 class Solution {
 public:
-    /*int findContentChildren(vector<int>& g, vector<int>& s) {
-        int n = g.size(), m = s.size();
-        
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int m = g.size(), n  = s.size();
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
 
-        int j = 0, count = 0;
-        for(int i=0; i<n && j<m ; i++)
-        {
-            if(i <= j && g[i] <= s[j])
-            {
-                count++;
-                j++;
-            }
-            else
-            {
-                while(j < m && g[i] > s[j]) 
-                    j++;
-                if(j < m){
-                    count++;
-                    j++;
-                }
-            }
-            
-        }
-        return count;
-    }*/
-
-    int findContentChildren(vector<int>& g, vector<int>& s)
-    {
-        int n = g.size(), m = s.size();
-        sort(g.begin(), g.end());
-        sort(s.begin(), s.end());
-
-        int i = 0, j = 0, count = 0;
-        while(i < n && j < m)
+        int i=0, j=0;
+        while(i < m && j < n)
         {
             if(g[i] <= s[j])
-            {
-                count++;
                 i++;
-            }
             j++;
         }
-
-        return count;
+        return i;
     }
 };
